68591bf1983dd551539362c9452298592f7b8f15
Julian Lamas-Rodriguez <julian@lamasrodriguez.com>
2024-04-02 21:28:13 +0200

    Add tests for index view

--- game/tictactoe/tests.py
+++ game/tictactoe/tests.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Board, FieldState
 
@@ -33,3 +36,32 @@ class BoardModelTests(TestCase):
         self.assertEqual(board.state, "OOXOXX  O")
         board.set_field_state(2, 2, FieldState.EMPTY)
         self.assertEqual(board.state, "OOXOXX   ")
+
+
+class IndexViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.password = "test"
+        cls.user1 = User.objects.create_user(username="test1", password=cls.password)
+        cls.user2 = User.objects.create_user(username="test2", password=cls.password)
+        cls.user3 = User.objects.create_user(username="test3", password=cls.password)
+        cls.board1 = Board.objects.create(
+            crosses_player=cls.user1, noughts_player=cls.user2
+        )
+        cls.board2 = Board.objects.create(
+            crosses_player=cls.user2, noughts_player=cls.user3
+        )
+        cls.board3 = Board.objects.create(
+            crosses_player=cls.user3, noughts_player=cls.user1
+        )
+
+    def test_boards_of_logged_user_are_displayed(self) -> None:
+        self.client.login(username=self.user1.username, password=self.password)
+        response = self.client.get(reverse("tictactoe:index"))
+        self.assertQuerySetEqual(
+            response.context["user_boards"], [self.board1, self.board3], ordered=False
+        )
+
+    def test_non_logged_users_see_no_boards(self) -> None:
+        response = self.client.get(reverse("tictactoe:index"))
+        self.assertQuerySetEqual(response.context["user_boards"], [], ordered=False)
